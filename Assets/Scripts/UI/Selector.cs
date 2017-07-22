8bb9daeac6ad67aa6fdde28bf0a46dc23be655ad
Vic <marsh.12th@gmail.com>
2017-07-22 02:20:57 +0200

    selector
    animators por red?

--- Assets/Scripts/UI/Selector.cs
+++ Assets/Scripts/UI/Selector.cs
@@ -10,14 +10,49 @@ public class Selector : NetworkBehaviour
 {
 	public Image current;
 	public Image next;
-	public Sprite[] personajes;			// El orden tiene que coincidir!
-	public GameObject focus;			// Marca cual es nuestro personje
+	public Sprite[] personajes;			// El orden tiene que coincidir con la enum!
+	public GameObject focus;            // Marca cual es nuestro personje
+
+	int charId;
+	bool sliding;
 
 	private void Update() 
 	{
 		if (!hasAuthority) return;
+		
+		/// En caso de que se pulse tecla de mover
+		/// ( y si no se está moviendo ya ),
+		/// deslizar targeta de personaje.
+		var dir = InputX.GetMovement ();
+		if (!sliding && dir != 0) StartCoroutine (Slide (dir));
+	}
+	private void Awake() 
+	{
+		charId = Game.id;
+		current.sprite = personajes[charId];
+	}
 
+	IEnumerator Slide ( float dir )
+	{
+		// Posicionar siguiente slide
+		next.rectTransform.position = current.rectTransform.position + Vector3.right * 100f * dir;
+		// Cambiar imagen en siguiente slide
+		CorrectSlideID (( int ) dir);
+		next.sprite = personajes[charId];
 
+		var progress = 0f;
+		while ( progress <= 1 )
+		{
+			yield return null;
+		}
+	}
+
+	void CorrectSlideID ( int dir )
+	{
+		charId += dir;
+		if (charId == -1) charId = personajes.Length-1;
+		else
+		if (charId == personajes.Length) charId = 0;
 	}
 }
 
@@ -26,9 +61,10 @@ public enum PJs
 	/// Lista de todos los personajes
 	/// que se pueden seleccionar
 	Gatete,
-	Random_1,
+	Sir,
 	Random_0,
-	Sir
+	Random_1
 	// En un futuro cuidado con
 	// cambiar los nombres!
+	// El orden tiene que coincidir con la array!
 }
