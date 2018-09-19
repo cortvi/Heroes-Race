61193c2ba89ee5c2b00f2145c5ccc33089aee633
Victor Navarro <victor.navarro.xyz@gmail.com>
2018-09-19 11:28:28 +0200

    aver si esto no peta

--- Assets/Scripts/Core/Selector.cs
+++ Assets/Scripts/Core/Selector.cs
@@ -42,10 +42,10 @@ namespace HeroesRace
 			// Blend the animator space for all instances
 			float blend = anim.GetFloat ("Blend");
 			float target = anim.GetInt ("Selection") / SelectionMax;
-			float lerp = Mathf.Lerp (blend, target, Time.deltaTime * 3f);
+			float lerp = Mathf.Lerp (blend, target, Time.deltaTime * 5f);
 			anim.SetFloat ("Blend", lerp);
 
-			if (isServer) closeEnough = Mathf.Abs(target - blend) <= 0.05f;
+			if (isServer) closeEnough = Mathf.Abs(target - blend) <= 0.025f;
 		}
 
 		protected override void OnAwake () 
@@ -103,6 +103,7 @@ namespace HeroesRace
 				anim.SetInt ("Selection", 2);
 				anim.SetFloat ("Blend", 1f / SelectionMax);
 			}
+			closeEnough = false;
 			
 			// Animator is owned by the client, so send the new Selection
 			Rpc_Move (selection);
